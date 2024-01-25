1e28b2bd39136a6e68ba3b5af5806946a27df210
Egwaoje Daniel <106787409+kikachukwu137@users.noreply.github.com>
2024-01-25 13:31:17 +0100

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
 const http = require("http")
 
+
 const PORT = 8900;
 const HOSTNAME = "localhost";
 
 
-// request handler or the behaviour 
-function requestHandler(res,res){
-    res.writeHead(200)
-    res.write("EGWAOJE DANIEL KIKACHUKWU")
-    res.end()
+// request handler
+
+
+function requestHandler(req,res){
+    if (req.method === "GET" && req.url ==="/books"){
+        console.log("you have enter the book route")
+    }
+    else if(req.method ==="PUT" && req.url ==="/books"){
+        console.log("book route have been updated")
+    }
+    else if(req.method === "DELETE" && req.url === "/books"){
+        console.log("book route have been deleted")
+    }
+    else if(req.method === "GET" && req.url === "/books/author"){
+        console.log("you have enter the author route")
+    }
+    else if (req.method === "POST"  && req.url === "/books/author"){
+        console.log("route auther have been created")
+    }
+    else if (req.method === "PUT" && req.url === "/books/author"){
+        console.log("author route have been updated")
+    }
+    else{
+
+        console.log("sorry an error have occurred")
+    }
 }
 
 
-// create a server
 
+// create a server
 const server = http.createServer(requestHandler)
 
-// start the server
-server.listen(PORT, HOSTNAME,()=>{
-    console.log(`server is running at http:${HOSTNAME}:${PORT}`)
+
+
+
+// start a server
+server.listen(PORT, HOSTNAME, ()=>{
+    console.log(`server is up and running at http://${HOSTNAME}:${PORT}`)
 })
+
